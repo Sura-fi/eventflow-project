7214089eb3828367dd45a2c4fa04d629dda0c476
Sura-fi <surafelbekele978@gmail.com>
2025-12-21 05:39:15 -0800

    Move requirements.txt into backend folder

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = 'django-insecure-o$mq61iyjayeyfvhebie7oalqm$3dp77pqh!b5g(ic2=on9wk5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'accounts.User'
 
